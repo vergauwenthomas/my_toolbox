87332122bff7fed02075a2c0e757de91ddea46ec
Thomas Vergauwen <cu1c@aa6-101.bullx>
2024-05-16 07:21:11 +0000

    proper argsparse for epywhat

--- scripts/epy_what.py
+++ scripts/epy_what.py
@@ -4,16 +4,36 @@ import argparse
 import os
 import epygram
 
-parser = argparse.ArgumentParser(description='EPYGRAM plotting as a python wrapper')
-parser.add_argument("file", help="FA filename of path.", default='') #argument without prefix
+def run_epygram_what(filepath):
+    assert os.path.isfile(fa_file), f'{args.file} not found.'
+    
+    epygram.init_env()
 
-args = parser.parse_args()
+    data = epygram.formats.resource(fa_file, 'r')
+    data.what() #Print all info
+    
 
-fa_file = str(args.file)
-assert os.path.isfile(fa_file), f'{args.file} not found.'
 
 
-epygram.init_env()
 
-data = epygram.formats.resource(fa_file, 'r')
-data.what() #Print all info
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(prog='What ??',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="""
+What ??: Print out an overview of what is contained by an FA file
+-------------------------------------------------------------------
+
+There are no special options for this script.
+"""
+)
+
+    parser.add_argument("file", help="FA filename, path of FA file or similar regex expression on filenames.", default='')  # argument without prefix
+
+    args = parser.parse_args()
+
+    # Run the application
+    run_epygram_what(str(args.file))
+
+
+
