19e5f9eaa5fcc5f2eb0d9eba257dd0d838d2636f
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2024-05-21 11:26:00 +0200

    test rsync transfer

--- scripts/telecom_tools.py
+++ scripts/telecom_tools.py
@@ -8,7 +8,9 @@ Created on Fri May 17 13:37:38 2024
 import datetime
 import pandas as pd
 import os
+import yaml
 from pathlib import Path
+
 
 toolbox_dir = Path(__file__).resolve().parents[1]
 telecom_base_dir = '/mnt/HDS_ALADIN/ALADIN/telecom'
@@ -19,8 +21,82 @@ telecom_base_dir = '/mnt/HDS_ALADIN/ALADIN/telecom'
 start="2020-08-02T02:00:00Z"
 end = "2020-08-02T03:00:00Z"
 
+
 # =============================================================================
-#
+# Helpers
+# =============================================================================
+
+def run_bash_in_python(cmd_list):
+    """ Run a bash command, the input is a list.
+    (The output and error are streamed to a print statement)
+    """
+
+    #Pipe output of command in real-time !!!!
+    with Popen(cmd_list, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:
+            print(line, end='') # process line here
+
+    if p.returncode != 0:
+        raise CalledProcessError(p.returncode, p.args)
+
+    return
+
+
+
+def get_secrets():
+    """ Read the secrets yaml file and return a dict of secrets."""
+    secretsfile = os.path.join(bashtools, 'secrets.yml')
+
+    with open(secretsfile) as stream:
+        try:
+            secrets = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return secrets
+
+# =============================================================================
+# TUNNEL: KILI --> ATOS
+# =============================================================================
+
+def creat_teleport_tunnel():
+    """ Setup the teleport login, using the secrets file. """
+    #lauch a bash script
+    run_bash_in_python(['source', 'bashtools/auth_to_atos.sh']) #Setup teleport
+
+    return
+
+def transfer_file(filepath, target_path, atos_login='aa-login'):
+    """ Transfer a file (from kili), to ATOS. """
+    #Read secrets
+    secrets = get_secrets()
+
+    cmd = ['rsync',
+           '-avu',
+           f'{filepath}',
+           f'{secrets["ecmwf_username"]}@{atos_login}:{target_path}']
+
+   run_bash_in_python(cmd)
+   return
+
+
+#%% testing
+print('creating tunnel')
+creat_teleport_tunnel()
+
+print('done with tunnel')
+file = "/home/thoverga/fileserver/home/error"
+target = "/home/cu1c/."
+
+transfer_file(file, target)
+
+
+print('file transferd')
+#%%
+
+
+
+# =============================================================================
+# KILI-SIDED: Construct paths to telecom files
 # =============================================================================
 
 
@@ -65,8 +141,6 @@ def construct_datetime_telecom_map(startstr, endstr,
     return dt_to_telecom_map
 
 
-test=construct_datetime_telecom_map(startstr=start,
-                                    endstr=end)
 
 
 
