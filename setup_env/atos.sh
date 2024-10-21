bf303f8c2e7e0a1ed4409d5150491f576e20c747
Thomas Vergauwen <cu1c@aa6-101.bullx>
2024-10-21 12:09:52 +0000

    tests on atos

--- setup_env/atos.sh
+++ setup_env/atos.sh
@@ -13,7 +13,13 @@ module load python3/3.10.10-01
 
 #install environment
 cd ..
+
+#develop phase:
+poetry update
 poetry install --no-root #create a poetry environment (but keep the package editable)
+
+#non-develop
+#poetry install
 
 #activate env
 source $(poetry env info --path)/bin/activate
